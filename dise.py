51fafe7a66068e0bf010987c36e9fa2b1446321c
xiangbaloud <xiangbaloud@gmail.com>
2017-03-20 15:36:10 +0800

    file modify

--- dise.py
+++ dise.py
@@ -1,11 +1,11 @@
 import random
-class Die(object):
+
+class Dise(object):
   def __init__(self, sides):
     self.sides = sides
   def roll(self):
     return random.randint(1, self.sides)
 
-d = Die(6)
-d1 = Die(6)
+d = Dise(6)
+d1 = Dise(6)
 print d.roll(), d1.roll()
-

ddc7be93ca29c172e27c8830e9207bcc6d827abd
xiangbaloud <xiangbaloud@gmail.com>
2017-06-23 14:02:42 +0800

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return 'FizzBuzz'
+    elif n % 3 == 0:
+        return 'Fizz'
+    elif n % 5 == 0:
+        return 'Buzz'
+    else:
+        return str(n)
+
+print ", ".join(fizzbuzz(n) for n in range(1, 100))
